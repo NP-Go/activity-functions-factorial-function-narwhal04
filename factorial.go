e74cf2ec5b1bdffdbdc1e9a5766dfbb1b9261457
narwhal04 <q.quek7@gmail.com>
2022-04-01 13:42:51 +0800

    fucntioning code

--- factorial.go
+++ factorial.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
 //Declare factorial function here
+func factorial(number int) int {
+	if number == 0 {
+		return 1
+	} else {
+		return number * factorial(number-1)
+	}
+}
 
 func main() {
 	//Insert Code here
+	fmt.Println("Input number")
+	var userInput int
+	fmt.Scan(&userInput)
+	fmt.Println(factorial(userInput))
 }
